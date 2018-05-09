3da16ded0d38cbad0ae18686cbe8fabe97edae1d
Chris022 <christoph02.cw@gmail.com>
2018-05-10 00:11:10 +0200

    Changed layouout

--- kivytut.py
+++ kivytut.py
@@ -1,23 +1,56 @@
-import kivy
-kivy.require('1.9.0')
- 
 from kivy.app import App
-from kivy.uix.pagelayout import PageLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+# Create both screens. Please note the root.manager.current: this is how
+# you can control the ScreenManager from kv. Each screen has by default a
+# property manager that gives you the instance of the ScreenManager used.
+Builder.load_file("hellokivy.kv")
+   
+
+
+
+
  
-# Inherit Kivy's App class which represents the window
-# for our widgets
-# HelloKivy inherits all the fields and methods
-# from Kivy
-class CalcPageLayout(PageLayout):
-    
-    def calc(self, text):
-        self.display.text += "ergebniss"
+class MenuScreen(Screen):
+    pass
+ 
+
+
+
+       
+class SettingsScreen(Screen):
+    pass
+
+
+
+
+
+class CreateScreen(Screen):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+# Create the screen manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(SettingsScreen(name='settings'))
+sm.add_widget(CreateScreen(name='create'))
+
 
 class HelloKivyApp(App):
  
     # This returns the content we want in the window
     def build(self):
-        return CalcPageLayout()
+        return sm
  
     
     
